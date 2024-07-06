0f647df9efc72d428175c1bcce2fa6fff84d1692
LOTTMAN OFFICIAL <LOTTMAN OFFICIAL@DESKTOP-8B315VK>
2024-07-06 07:45:03 -0700

    completed interface

---
+++ Interfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// Interface IMovable
+public interface IMovable
+{
+    void Move();
+}
+
+// Class Car that implements IMovable
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class Bicycle that implements IMovable
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances of Car and Bicycle
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        // Call the Move() method on each instance
+        car.Move();
+        bicycle.Move();
+
+        Console.ReadLine();
+    }
+}
